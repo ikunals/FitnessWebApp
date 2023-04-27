7574e15d876ad67a15b1ac27f9781495e906b66e
kunal <kunalsawant4142@gmail.com>
2023-04-27 11:38:56 +0530

    app_api

---
+++ APP_API/middleware/authUser.js
@@ -0,0 +1,34 @@
+var jwt = require('jsonwebtoken');
+const JWT_SECRET = 'sweat4FitAPI';
+
+const authUser = (req, res, next)=>{
+    // Get the user from the JWT Token and add id to req objec
+    console.log('req: ',req);
+    const authtoken = req.header('Authorization');
+    console.log('res: ',authtoken);    
+    const token = authtoken.replace('Bearer ','');
+    // const TokenSplit = authtoken.split(" ");
+    // const token = TokenSplit[1];
+
+    console.log("token =" + token);
+    if(!token){
+        res.status(401).send({
+            error: "Please authenticate using a valid token"
+        })
+    }
+    try {
+        console.log("inside try");
+        const data = jwt.verify(token, JWT_SECRET)
+        req.user = data.id;
+        next();
+    }
+    catch (error) {
+        res.send(401).send({
+            error: "No token"
+        })
+    }
+   
+
+}
+
+module.exports = authUser;
